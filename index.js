edea30d623ac446985da1b024420190c3265bb5a
jiancaballero <jiancaballero@gmail.com>
2022-11-07 18:42:26 +0800

    backend deployment

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const port = process.env.PORT || 3000
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// IMPORT BODYPARSER
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+// API Routes
+const roomRoute = require("./routes/room");
+const bookingRoute = require("./routes/booking");
+
+// API Endpoints
+app.use("/api/rooms", roomRoute);
+app.use("/api/bookings", bookingRoute);
+// app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Express server running on port ${port}`);
+});
