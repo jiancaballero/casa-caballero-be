c9f2d22e5427a105fe0e38b2ed960bf47cbfdad1
jiancaballero <jiancaballero@gmail.com>
2022-11-13 02:44:29 +0800

    fixing cors origin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://casa-caballero-be.herokuapp.com/",
   credentials: true,
   optionSuccessStatus: 200,
 };
