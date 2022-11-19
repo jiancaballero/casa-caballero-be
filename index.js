f206b12964c20855123452ad017d603ac1a7bd75
jiancaballero <jiancaballero@gmail.com>
2022-11-20 02:03:35 +0900

    add timeouts

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const port = process.env.PORT || 3000
 const app = express();
 const cors = require("cors");
+var timeout = require('connect-timeout')
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: false,
   optionSuccessStatus: 200,
 };
 app.use(cors);
-
+app.use(timeout('5s'))
+app.use(haltOnTimedout)
 // IMPORT BODYPARSER
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -24,7 +26,9 @@ const bookingRoute = require("./routes/booking");
 app.use("/api/rooms", roomRoute);
 app.use("/api/bookings", bookingRoute);
 // app.use(express.json());
-
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
 });
